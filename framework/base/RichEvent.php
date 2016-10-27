3899535e7edc85765038165f221019f2b739fa83
Nils <deele@tuta.io>
2016-10-28 01:35:22 +0300

    Addition of getContextData()

--- framework/base/RichEvent.php
+++ framework/base/RichEvent.php
@@ -1,9 +1,7 @@
 <?php
-/**
- * @link http://www.yiiframework.com/
- */
-
 namespace yii\base;
+
+use yii\helpers\ArrayHelper;
 
 /**
  * RichEvent allows creation of events that carry contextual data.
@@ -12,8 +10,22 @@ namespace yii\base;
  */
 class RichEvent extends Event
 {
+
     /**
      * @var array with contextual data. Defaults to empty array.
      */
     public $contextData = [];
+
+    /**
+     * Retrieves the value from context data array
+     *
+     * @param string $key
+     * @param mixed $default
+     *
+     * @return mixed
+     */
+    public function getContextData($key, $default = null)
+    {
+        return ArrayHelper::getValue($key, $default);
+    }
 }
